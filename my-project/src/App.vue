c904042f2496bc20620b1bfdafacf32aac76936a
monsichasris <monsicha.srisuantang@gmail.com>
2025-03-29 12:56:36 -0400

    set up project

--- my-project/src/App.vue
+++ my-project/src/App.vue
@@ -1,15 +1,21 @@
 <template>
-  <img alt="Vue logo" src="./assets/cat.png" width="200px"/>
-  <HelloWorld msg="Welcome to Major Studio" />
+  <img alt="title" src="./assets/title.png" width="100%" />
+  <Intro
+    text="If you live in the city, going to the grocery store is a part of
+      life—whether you enjoy it or not. But have you ever really looked at the
+      signs? Noticed something unusual? Felt drawn to one in particular? These
+      signs tell a story more than you think."
+  />
+  <img src="./assets/basket.svg" />
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import Intro from "./components/Intro.vue";
 
 export default {
   name: "App",
   components: {
-    HelloWorld,
+    Intro,
   },
 };
 </script>
@@ -21,6 +27,12 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  margin-top: 0;
+}
+.textbox {
+  font-size: 16px;
+  line-height: 1.5;
+  margin: 10% auto;
+  width: 400px;
 }
 </style>
---
+++ my-project/src/components/Intro.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="intro">
+    <p class="textbox">{{ text }}</p>
+    <!-- <p class="textbox">
+      If you live in the city, going to the grocery store is a part of
+      life—whether you enjoy it or not. But have you ever really looked at the
+      signs? Noticed something unusual? Felt drawn to one in particular? These
+      signs tell a story more than you think.
+    </p>
+    <p class="textbox">
+      For me, exploring grocery stores is a way to understand a place, what
+      people eat and how they live. In NYC, there are over 10,000 grocery
+      stores, each reflecting different foods, cuisines, and cultures. What do
+      they reveal about the people here?<br />
+      Let’s take a closer look.
+    </p> -->
+  </div>
+</template>
+
+<script>
+export default {
+  name: "IntroSection",
+  props: {
+    text: String,
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.intro {
+  height: 50vh;
+}
+</style>
