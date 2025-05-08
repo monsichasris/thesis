05cd9cc81bfd32806b410c5e5d8c44e9058337ce
monsichasris <monsicha.srisuantang@gmail.com>
2025-05-07 21:49:07 -0400

    add lottie

--- src/components/Intro.vue
+++ src/components/Intro.vue
@@ -1,8 +1,6 @@
 <template>
   <div>
-    <img
-      src="img/title.svg"
-      width="50%"
+    <div
       style="
         position: absolute;
         top: 0;
@@ -10,24 +8,22 @@
         top: 30%;
         transform: translateX(-50%);
         z-index: 2;
+        display: flex;
+        flex-direction: column;
+        gap: 24px;
       "
-    />
+    >
+      <img src="img/title.svg" width="100%" style="" />
+      <div class="lottie-container" ref="lottieContainer"></div>
+    </div>
     <img
       src="img/title_basket.svg"
-      width="80%"
+      height="100%"
       style="position: absolute; top: 0; left: 50%; transform: translateX(-50%)"
     />
   </div>
 
   <MatterAnimation :basketSvg="'img/baseline.svg'" :images="images" />
-  <!-- <div class="title">
-    <img alt="grocery signery" src="img/title.svg" width="50%" />
-    <img alt="stores" src="img/stores.png" width="100%" />
-    <div>
-      <p class="skew subhead">Scenery Behind NYC Storefront Signs</p>
-      <img alt="scroll down" src="img/chevron-down.svg" />
-    </div>
-  </div> -->
 
   <div style="height: 100vh; position: sticky; top: 0">
     <img
@@ -45,13 +41,17 @@
   <div class="scroll">
     <p class="textbox">
       If you live in the city, going to the grocery store is a part of life,
-      whether you enjoy it or not. <br /><br />
+      whether you enjoy it or not.
+    </p>
+    <p class="textbox">
       Exploring grocery stores is one of the ways to understand a place and the
-      people who live there. <br /><br />
+      people who live there.
+    </p>
+    <img src="img/people.png" width="100%" />
+    <p class="textbox">
       In NYC, there are almost 10,000 grocery stores, each reflecting different
       foods, cuisines, and cultures
     </p>
-    <!-- <img src="img/samples-img.png" style="transform: scale(0.8)" /> -->
     <p class="textbox">
       But before you ever walk in... <br />
       have you ever really looked at the
@@ -79,6 +79,7 @@
 
 <script>
 import MatterAnimation from "./MatterAnimation.vue";
+import lottie from "lottie-web";
 export default {
   name: "IntroSection",
   components: { MatterAnimation },
@@ -89,6 +90,15 @@ export default {
     return {
       images: this.loadImages(),
     };
+  },
+  mounted() {
+    lottie.loadAnimation({
+      container: this.$refs.lottieContainer, // The container where the animation will be rendered
+      renderer: "svg", // Render as SVG
+      loop: true, // Loop the animation
+      autoplay: true, // Start playing automatically
+      path: "/img/chevron-down.json", // Path to your Lottie JSON file
+    });
   },
   methods: {
     loadImages() {
@@ -119,5 +129,15 @@ export default {
   align-items: center;
   gap: 5vh;
   padding-top: 10vh;
+}
+
+.lottie-container {
+  width: 64px;
+  height: 64px;
+  margin: 0 auto;
+}
+
+.textbox {
+  margin: 400px auto;
 }
 </style>
