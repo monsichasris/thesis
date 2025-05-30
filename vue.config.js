831c45096d7249020e9e80be84392c0fcaee6266
monsichasris <monsicha.srisuantang@gmail.com>
2025-05-30 14:59:36 -0400

    split chunks

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,26 @@
 const { defineConfig } = require("@vue/cli-service");
+const CompressionPlugin = require("compression-webpack-plugin");
 module.exports = defineConfig({
   transpileDependencies: true,
-});
-module.exports = {
   publicPath: process.env.NODE_ENV === "production" ? "/thesis/" : "/",
-};
+  chainWebpack: (config) => {
+    config.optimization.splitChunks({
+      chunks: "all",
+      maxSize: 250000, // Split chunks larger than 250 KiB
+    });
+  },
+  configureWebpack: {
+    performance: {
+      hints: process.env.NODE_ENV === "production" ? "warning" : false,
+      maxEntrypointSize: 512000,
+      maxAssetSize: 512000,
+    },
+    plugins: [
+      new CompressionPlugin({
+        test: /\.(js|css|json|html|svg)$/, // Compress these file types
+        threshold: 10240, // Only compress files larger than 10 KiB
+        minRatio: 0.8, // Compress if the compression ratio is better than 0.8
+      }),
+    ],
+  },
+});
