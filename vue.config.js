2dc8e0b172ab778d1525895e18ab520536eefd6f
monsichasris <monsicha.srisuantang@gmail.com>
2025-05-30 18:22:12 -0400

    remove plugin

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,4 @@
 const { defineConfig } = require("@vue/cli-service");
-const { BundleAnalyzerPlugin } = require("webpack-bundle-analyzer");
 const CompressionPlugin = require("compression-webpack-plugin");
 module.exports = defineConfig({
   transpileDependencies: true,
@@ -21,10 +20,6 @@ module.exports = defineConfig({
         test: /\.(js|css|json|html|svg)$/, // Compress these file types
         threshold: 10240, // Only compress files larger than 10 KiB
         minRatio: 0.8, // Compress if the compression ratio is better than 0.8
-      }),
-      new BundleAnalyzerPlugin({
-        analyzerMode: "static", // Generates a static HTML file
-        openAnalyzer: true, // Automatically opens the report in your browser
       }),
     ],
   },
